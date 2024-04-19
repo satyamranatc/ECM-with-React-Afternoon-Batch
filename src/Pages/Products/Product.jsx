103ad6305578b7a9c725345c534670c4049dd2d5
Satyam Rana <57563008+satyamranatc@users.noreply.github.com>
2024-04-19 15:15:33 +0530

    done

--- src/Pages/Products/Product.jsx
+++ src/Pages/Products/Product.jsx
@@ -4,24 +4,36 @@ import "./Product.css"
 
 export default function Product() {
   let [Allproducts, setAllProducts] = useState([]);
+  let [Filteredproducts, setFilteredProducts] = useState([]);
 
   let GetProductData = async () => {
     let data = await fetch("https://api.escuelajs.co/api/v1/products");
     data = await data.json();
     setAllProducts(data);
+    setFilteredProducts(data);
   }
 
   useEffect(() => {
     GetProductData();
+   
   }, []);
+
+  function CahngeCategory(cat)
+  {
+
+    setFilteredProducts(Allproducts.filter((e)=>e.category.name === cat));
+  }
 
   return (
     <div>
       <center>
         <h1>Product</h1>
+        <button onClick={()=>CahngeCategory("Electronics")}>Electronics</button>
+        <button onClick={()=>CahngeCategory("Clothes")}>Cloths</button>
         <hr />
         <div id="Cards">
-        {Allproducts.map((product, index) => (
+        {Filteredproducts.map((product, index) => (
+          console.log(product),
           <ProductCard
             key={index}
             title={product.title}
